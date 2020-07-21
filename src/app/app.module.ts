d4146d8f0dfb92f91c93e6e1bfa0a7000676ce37
nguyentrongdoan <nguyendoan206@gmail.com>
2020-07-21 15:54:17 +0700

    thêm component form-login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormLoginComponent } from './form-login/form-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormLoginComponent
   ],
   imports: [
     BrowserModule,
