13aba4fa8daf0e90afa6e581b83f5e1cff95d167
nguyentrongdoan <nguyendoan206@gmail.com>
2020-07-23 16:08:02 +0700

    thêm datepipe vào form-login.ts

---
+++ src/app/service/api.service.ts
@@ -0,0 +1,20 @@
+import {Injectable} from '@angular/core';
+import {environment} from '../../environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {Observable, Subject} from 'rxjs';
+import {IUser} from '../model/User';
+
+const apiUrl = environment.apiSource;
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  shouldRefresh = new Subject<any>();
+
+  constructor(private httpClient: HttpClient) {
+    console.log(environment.apiSource);
+  }
+  createUser(user: IUser): Observable<IUser> {
+    return this.httpClient.post<IUser>(apiUrl + '/auth/register', user);
+  }
+}
