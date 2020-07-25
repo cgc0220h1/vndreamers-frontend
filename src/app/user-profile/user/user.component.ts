da1d72b6ea72b5cdd29e49b4baac1a4abbdfdd19
Duc <vanduc2514@gmail.com>
2020-07-25 14:16:13 +0700

    Tách component và cho nút edit profile

--- src/app/user-profile/user/user.component.ts
+++ src/app/user-profile/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -6,9 +6,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  openEditProfileDialog(): void {
+  }
 }
