6c6fc593b93b6bbe6e6e5df40f47f1612563bbd1
Duc <vanduc2514@gmail.com>
2020-07-25 12:03:51 +0700

    Thêm dependency, config để import template vào dự án

--- src/app/user-profile/user-profile.module.ts
+++ src/app/user-profile/user-profile.module.ts
@@ -3,14 +3,31 @@ import {CommonModule} from '@angular/common';
 
 import {UserProfileRoutingModule} from './user-profile-routing.module';
 import {WallComponent} from './wall/wall.component';
-import { UserInfoComponent } from './user-info/user-info.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatButtonModule} from '@angular/material/button';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {UserComponent} from './post/user.component';
 
 
 @NgModule({
-  declarations: [WallComponent, UserInfoComponent],
+  declarations: [WallComponent, UserComponent],
   imports: [
     CommonModule,
-    UserProfileRoutingModule
+    UserProfileRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatTabsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatRadioModule,
+    MatDatepickerModule,
+    FormsModule
   ]
 })
 export class UserProfileModule {
