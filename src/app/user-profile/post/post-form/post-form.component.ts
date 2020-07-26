e67dcb6bcd5a91760975df68c3c127ad7608281b
Duc <vanduc2514@gmail.com>
2020-07-27 00:05:05 +0700

    Tái cấu trúc tính năng comment Thêm validate cho nội dung post

--- src/app/user-profile/post/post-form/post-form.component.ts
+++ src/app/user-profile/post/post-form/post-form.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {IUser} from '../../../model/User';
 import {PostService} from '../../../service/post.service';
 import {MatSnackBar} from '@angular/material/snack-bar';
@@ -12,7 +12,7 @@ import {MatSnackBar} from '@angular/material/snack-bar';
 export class PostFormComponent implements OnInit {
   postForm: FormGroup = new FormGroup({
     status: new FormControl(''),
-    content: new FormControl(''),
+    content: new FormControl('', Validators.required),
     image: new FormControl('')
   });
 
@@ -29,17 +29,24 @@ export class PostFormComponent implements OnInit {
   }
 
   onSubmit(): void {
-    this.postForm.value.status = 1;
-    this.postService.createPost(this.postForm.value).subscribe(result => {
-      this.snackBar.open('Post bài thành công', '', {
-        duration: 2500
+    this.postForm.markAllAsTouched();
+    if (this.postForm.valid) {
+      this.postForm.value.status = 1;
+      this.postService.createPost(this.postForm.value).subscribe(result => {
+        this.snackBar.open('Post bài thành công', '', {
+          duration: 2500
+        });
+        this.postService.shouldRefresh.next(result);
+      }, error => {
+        this.snackBar.open('Post bài không thành công', '', {
+          duration: 2500
+        });
+        console.log(error);
       });
-      this.postService.shouldRefresh.next(result);
-    }, error => {
-      this.snackBar.open('Post bài không thành công', '', {
-        duration: 2500
+    } else {
+      this.snackBar.open('Nội dung không được để trống!', '', {
+        duration: 1000
       });
-      console.log(error);
-    });
+    }
   }
 }
