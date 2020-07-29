67e8c57ee649f6d9233cf6ae91637370f2da709b
phi dao <daovanphi25051994@gmail.com>
2020-07-29 14:14:36 +0700

    Hoan thanh upload file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,14 @@
 export const environment = {
   production: true,
-  apiSource: 'https://vndreamers-backend.herokuapp.com'
+  apiSource: 'https://vndreamers-backend.herokuapp.com',
+  firebaseConfig : {
+    apiKey: 'AIzaSyCwKcQqWyLdSwNL9wdjJJ1OdQ5UjLXWd0Y',
+    authDomain: 'vndreamer-fontend.firebaseapp.com',
+    databaseURL: 'https://vndreamer-fontend.firebaseio.com',
+    projectId: 'vndreamer-fontend',
+    storageBucket: 'vndreamer-fontend.appspot.com',
+    messagingSenderId: '971529387658',
+    appId: '1:971529387658:web:d8ee4c923003ff1fb53773',
+    measurementId: 'G-SJFGQCF71D'
+  }
 };
