dfec568ba8a637625341073fda7934f039835bc6
Duc <vanduc2514@gmail.com>
2020-07-31 02:00:57 +0700

    Fix Logic Upload File sao cho dễ đọc và dễ xử lý hơn

--- src/app/user-profile/post/post-form/post-form.component.ts
+++ src/app/user-profile/post/post-form/post-form.component.ts
@@ -7,7 +7,7 @@ import {FileUpload} from '../../../model/upload-file';
 import {HttpClient} from '@angular/common/http';
 import {UploadFileService} from '../../../service/public/upload-file.service';
 import {IPost} from '../../../model/Post';
-import {Observable, Subject} from 'rxjs';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-post-form',
@@ -35,9 +35,9 @@ export class PostFormComponent implements OnInit {
   file: any;
   imageFile: any;
   selectedImage: FileList;
-  currentImageUpload: FileUpload;
+  imageToUpload: FileUpload;
+  imageUploadUrl: string;
   percentage: number;
-  url: string | ArrayBuffer = '';
   isShowProgressBar = false;
 
   ngOnInit(): void {
@@ -52,15 +52,13 @@ export class PostFormComponent implements OnInit {
 
   onSubmit(): void {
     this.postForm.markAllAsTouched();
-
-    this.uploadFileService.uploadSubject.subscribe(downloadUrl => {
-      this.postForm.value.image = downloadUrl;
+    if (this.postForm.valid && !this.isShowProgressBar) {
       this.postService.createPost(this.postForm.value).subscribe(result => {
         this.snackBar.open('Post bài thành công', '', {
           duration: 2500
         });
         this.whenClickPostButton.emit(result);
-        this.url = '';
+        this.imageUploadUrl = '';
         this.postForm.reset();
         this.postForm.get('status').setValue('1');
       }, () => {
@@ -68,47 +66,44 @@ export class PostFormComponent implements OnInit {
           duration: 2500
         });
       });
-    });
-
-    if (this.postForm.valid) {
-      if (this.selectedImage === undefined) {
-        this.uploadFileService.uploadSubject.next('');
-        this.uploadFileService.uploadSubject = new Subject();
-      } else {
-        this.isShowProgressBar = true;
-        this.upload().subscribe(next => {
-          this.percentage = Math.round(next * 100) / 100;
-        }, () => {
-          this.snackBar.open('Có lỗi xảy ra', '', {
-            duration: 2500
-          });
-        }, () => {
-          this.isShowProgressBar = false;
-        });
-      }
-
-    } else {
+    } else if (!this.isShowProgressBar) {
       this.snackBar.open('Nội dung không được để trống!', '', {
+        duration: 1000
+      });
+    } else {
+      this.snackBar.open('Vui lòng chờ File Upload xong!', '', {
         duration: 1000
       });
     }
   }
 
-  displayImage(event): void {
+  uploadImage(event): void {
     if (event.target.files && event.target.files[0]) {
       const reader = new FileReader();
-      reader.readAsDataURL(event.target.files[0]); // read file as data url
-      reader.onload = (fileEvent: ProgressEvent<FileReader>) => { // called once readAsDataURL is completed
-        this.url = fileEvent.target.result;
-      };
+      reader.readAsDataURL(event.target.files[0]);
+      this.selectedImage = event.target.files;
+      this.isShowProgressBar = true;
+
+      this.upload().subscribe(next => {
+        this.percentage = Math.round(next * 100) / 100;
+      }, () => {
+        this.snackBar.open('Có lỗi xảy ra', '', {
+          duration: 2500
+        });
+      });
+
+      this.uploadFileService.uploadSubject.subscribe((downloadUrl: string) => {
+        this.imageUploadUrl = downloadUrl;
+        this.isShowProgressBar = false;
+        this.postForm.get('image').setValue(downloadUrl);
+      });
     }
-    this.selectedImage = event.target.files;
   }
 
   upload(): Observable<any> {
     this.imageFile = this.selectedImage.item(0);
     this.selectedImage = undefined;
-    this.currentImageUpload = new FileUpload(this.imageFile);
-    return this.uploadFileService.pushFileToStorage(this.currentImageUpload);
+    this.imageToUpload = new FileUpload(this.imageFile);
+    return this.uploadFileService.pushFileToStorage(this.imageToUpload);
   }
 }
