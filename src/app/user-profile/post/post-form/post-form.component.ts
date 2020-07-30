d1e6049d596505050b08789e2ba479633ba25d09
phi dao <daovanphi25051994@gmail.com>
2020-07-30 09:34:30 +0700

    Reset status if post success

--- src/app/user-profile/post/post-form/post-form.component.ts
+++ src/app/user-profile/post/post-form/post-form.component.ts
@@ -12,98 +12,100 @@ const FRONT_LINK = 'https://firebasestorage.googleapis.com/v0/b/vndreamer-fonten
 const BACK_LINK = '?alt=media&token=1888bbc5-f913-4e94-90a5-b35aad7318c8';
 
 @Component({
-    selector: 'app-post-form',
-    templateUrl: './post-form.component.html',
-    styleUrls: ['./post-form.component.scss']
+  selector: 'app-post-form',
+  templateUrl: './post-form.component.html',
+  styleUrls: ['./post-form.component.scss']
 })
 
 
 export class PostFormComponent implements OnInit {
-    status = new FormControl('1');
-    postForm: FormGroup = new FormGroup({
-        status: this.status,
-        content: new FormControl('', Validators.required),
-        image: new FormControl('')
+  status = new FormControl('1');
+  postForm: FormGroup = new FormGroup({
+    status: this.status,
+    content: new FormControl('', Validators.required),
+    image: new FormControl('')
+  });
+
+  @Input() currentUser: IUser;
+  @Input() userRequest: IUser;
+
+  file: any;
+  imageFile: any;
+  selectedImage: FileList;
+  currentImageUpload: FileUpload;
+  percentage: number;
+  url: string | ArrayBuffer = '';
+
+  constructor(private postService: PostService,
+              private snackBar: MatSnackBar,
+              private fb: FormBuilder,
+              private http: HttpClient,
+              private uploadFileService: UploadFileService,
+              private storage: AngularFireStorage) {
+  }
+
+  ngOnInit(): void {
+    this.postForm.get('status').setValue('1');
+    this.postService.shouldRefresh.subscribe(result => {
+      this.postForm.reset();
     });
+  }
 
-    @Input() currentUser: IUser;
-    @Input() userRequest: IUser;
-
-    file: any;
-    imageFile: any;
-    selectedImage: FileList;
-    currentImageUpload: FileUpload;
-    percentage: number;
-    url: string | ArrayBuffer = '';
-
-    constructor(private postService: PostService,
-                private snackBar: MatSnackBar,
-                private fb: FormBuilder,
-                private http: HttpClient,
-                private uploadFileService: UploadFileService,
-                private storage: AngularFireStorage) {
+  onSubmit(): void {
+    if (this.imageFile !== undefined) {
+      this.upload();
+      this.setDefaultValue();
     }
-
-    ngOnInit(): void {
-        this.postService.shouldRefresh.subscribe(result => {
-            this.postForm.reset();
+    this.postForm.markAllAsTouched();
+    if (this.postForm.valid) {
+      this.postService.createPost(this.postForm.value).subscribe(result => {
+        this.snackBar.open('Post bài thành công', '', {
+          duration: 2500
         });
+        this.postService.shouldRefresh.next(result);
+        this.url = '';
+        this.postForm.get('status').setValue('1');
+      }, error => {
+        this.snackBar.open('Post bài không thành công', '', {
+          duration: 2500
+        });
+        console.log(error);
+      });
+    } else {
+      this.snackBar.open('Nội dung không được để trống!', '', {
+        duration: 1000
+      });
     }
+  }
 
-    onSubmit(): void {
-        if (this.imageFile !== undefined) {
-            this.upload();
-            this.setDefaultValue();
-        }
-        this.postForm.markAllAsTouched();
-        if (this.postForm.valid) {
-            this.postService.createPost(this.postForm.value).subscribe(result => {
-                this.snackBar.open('Post bài thành công', '', {
-                    duration: 2500
-                });
-                this.postService.shouldRefresh.next(result);
-                this.url = '';
-            }, error => {
-                this.snackBar.open('Post bài không thành công', '', {
-                    duration: 2500
-                });
-                console.log(error);
-            });
-        } else {
-            this.snackBar.open('Nội dung không được để trống!', '', {
-                duration: 1000
-            });
-        }
+  displayImage(event): void {
+    if (event.target.files && event.target.files[0]) {
+      const reader = new FileReader();
+      reader.readAsDataURL(event.target.files[0]); // read file as data url
+      // tslint:disable-next-line:no-shadowed-variable
+      reader.onload = (event) => { // called once readAsDataURL is completed
+        this.url = event.target.result;
+      };
     }
+    this.selectedImage = event.target.files;
+  }
 
-    displayImage(event): void {
-        if (event.target.files && event.target.files[0]) {
-            const reader = new FileReader();
-            reader.readAsDataURL(event.target.files[0]); // read file as data url
-            // tslint:disable-next-line:no-shadowed-variable
-            reader.onload = (event) => { // called once readAsDataURL is completed
-                this.url = event.target.result;
-            };
-        }
-        this.selectedImage = event.target.files;
-    }
+  setDefaultValue(): void {
+    this.postForm.value.image = FRONT_LINK + this.imageFile.name + BACK_LINK;
+  }
 
-    setDefaultValue(): void {
-        this.postForm.value.image = FRONT_LINK + this.imageFile.name + BACK_LINK;
-    }
-
-    upload(): void {
-        this.imageFile = this.selectedImage.item(0);
-        this.selectedImage = undefined;
-        this.currentImageUpload = new FileUpload(this.imageFile);
-        this.uploadFileService.pushFileToStorage(this.currentImageUpload).subscribe(
-            percentage => {
-                this.percentage = Math.round(percentage);
-            },
-            error => {
-                console.log(error);
-            }
-        );
-    }
+  upload(): void {
+    this.imageFile = this.selectedImage.item(0);
+    this.selectedImage = undefined;
+    this.currentImageUpload = new FileUpload(this.imageFile);
+    this.uploadFileService.pushFileToStorage(this.currentImageUpload).subscribe(
+      percentage => {
+        this.percentage = Math.round(percentage);
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
 
 }
