81b1a7166d24045c7b22ae692067a1fb67cb47ca
Duc <vanduc2514@gmail.com>
2020-07-30 15:32:47 +0700

    Sửa thiết kế navbar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,6 +6,8 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {AuthModule} from '../auth/auth.module';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 
 
@@ -18,6 +20,8 @@ import {MatIconModule} from '@angular/material/icon';
     AuthModule,
     MatButtonModule,
     MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   exports: [NavbarComponent]
 })
